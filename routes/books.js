9b2d06e35915e7b4389a8ce9bd5a8867d0b479f3
Radja <bouzidradjaa@gmail.com>
2026-04-11 23:46:09 +0200

    update books API and add frontend with header/navbar components

--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,29 @@
 const express=require("express")
 const router=express.Router()
-
-
 const book = require("../models/bookSchema")
+const multer  = require('multer')
 
 
-router.post("/createbook", async(req, res)=>{
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/mesimages/images')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, filename)
+  }
+})
+
+const upload = multer({ storage: storage })
+
+
+
+
+
+
+
+router.post("/createbook", upload.single('coverImage'), async(req, res)=>{
     try {
 const {title,author, description,price,stock,isfeautred,
         category, discountPercent, isOnSale}= req.body
@@ -24,6 +42,7 @@ const {title,author, description,price,stock,isfeautred,
              category,
              discountPercent,
              isOnSale,
+             coverImage : req.file?.filename , 
 
         })
         await newbook.save()
@@ -51,19 +70,54 @@ const {title,author, description,price,stock,isfeautred,
    
      router.get("/:id", async(req,res)=>{
         try {
-        const Book = await book.findById(req.params.id)
+        const book = await book.findById(req.params.id)
         if(!book){
             return res.status(404).json({message :"book is not founds"})
         }
 
-        return res.json(book)
+        return res.json(Book)
         } catch (error) {
          res.status(500).json({error: error.message})
 
    } 
 
     })
-    
+     router.put("/updatebook/:id" , async(req,res)=>{
+        try {
+            
+
+            const books = await book.findByIdAndUpdate(req.params.id,
+               req.body,
+               {new : true}
+            ).populate("category", "name")
+
+            if(!book){
+            return res.status(404).json({message :"book is not founds"})
+        }
+        res.json({message: "book updated successfully "})
+
+
+        } catch (error) {
+          res.status(500).json({error: error.message})
+
+        }
+     })
+
+     router.delete("/deletebook/:id", async(req,res)=>{
+        try {
+
+          const books = await book.findByIdAndDelete(req.params.id)
+        if(!book){
+            return res.status(404).json({message :"book is not founds"})
+        }
+         res.json({message: "book deleted successfully "})
+  
+            
+        } catch (error) {
+            
+        }
+        
+     })
    
 
     module.exports =router;
